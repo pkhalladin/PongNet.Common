859862a87ac35580e5d9139f3b54b8f4682e8cd3
Pawel Halladin <pawelhalladin@gmail.com>
2023-06-14 19:10:41 +0200

    Klasa Default

---
+++ Default.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace PongNet.Common
+{
+	public static class Default
+	{
+		public static readonly Color PrimaryColor = Color.White;
+		public static readonly Color SecondaryColor = Color.Black;
+	}
+}
